ff255f47b145471a3a10c9218674a7ca95092977
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-04-07 08:58:51 +0530

    Added : Welcome message

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    /**
+     * To display the welcome message on master branch
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to employee wage computation program!");
+    }
+}
