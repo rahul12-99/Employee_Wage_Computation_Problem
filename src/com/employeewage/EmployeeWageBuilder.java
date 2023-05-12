93ebf5603b3dc53f427ae1702038240163060fa4
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-05-12 17:24:25 +0530

    Refactor : One employeeWage Builder implements interface

--- src/com/employeewage/EmployeeWageBuilder.java
+++ src/com/employeewage/EmployeeWageBuilder.java
@@ -1,73 +1,69 @@
 package com.employeewage;
 
 
-public class EmployeeWageBuilder {
+public class EmployeeWageBuilder implements IEmployeeWage{
     /**
      * Initialize the variable
      */
-    public static final int IS_PART_TIME = 2;
-    public static final int IS_FULL_TIME = 1;
-    private int noOfCompany = 0;
-    private final CompanyEmployeeWage[] companyEmployeeWage;
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
 
-    /*
-     created constructor for passing size in array for company
+    private int numOfCompany = 0;
+    CompanyEmployeeWage[] companyEmpWageArray = new CompanyEmployeeWage[5];
+
+    /**
+     * This method is for adding company employee wage for each individual company taking it in array
      */
-    public EmployeeWageBuilder() {
-        companyEmployeeWage = new CompanyEmployeeWage[5];
+    @Override
+    public void addCompanyEmployeeWage(String Company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+        companyEmpWageArray[numOfCompany] = new CompanyEmployeeWage(Company, empRatePerHour, numOfWorkingDays, maxHoursPerMonth);
+        numOfCompany++;
     }
 
-    /*
-     This method for adding the company employee wage and passing variable as an arguments
+    /**
+     * This method is for compute employeeWage for multiple company
      */
-    private void addCompanyEmpWage(String company, int ratePerHour, int noOfWorkingDays, int maxWorkingHours) {
-        companyEmployeeWage[noOfCompany] = new CompanyEmployeeWage(company, ratePerHour, noOfWorkingDays, maxWorkingHours);
-        noOfCompany++;
-    }
-
-    /*
-     This method is for get monthly wage for each company
-     */
-    private void getMonthlyWages() {
-        for (int i = 0; i < noOfCompany; i++) {
-            companyEmployeeWage[i].setTotalEmpWage(this.getMonthlyWage(companyEmployeeWage[i]));
-            System.out.println(companyEmployeeWage[i]);
+    @Override
+    public void computeEmployeeWage() {
+        for (int i = 0; i < numOfCompany; i++) {
+            companyEmpWageArray[i].setTotalEmpWage(this.computeEmployeeWage(companyEmpWageArray[i]));
+            System.out.println(companyEmpWageArray[i]);
         }
     }
-    /*
-     This method is for getMonthly wage for all individual company and returning the totalWage
-     */
 
-    public int getMonthlyWage(CompanyEmployeeWage companyWage) {
-        int totalEmpHours = 0, totalWorkingDays = 0;
-        while (totalEmpHours < companyWage.maxWorkingHours && totalWorkingDays < companyWage.noOfWorkingDays) {
+    /**
+     * This method is for compute employeeWage for each individual company with part-time and full-time
+     */
+    private int computeEmployeeWage(CompanyEmployeeWage companyEmpWage) {
+        int empHrs , totalWorkingDays = 0, totalEmpHrs = 0;
+        while (totalEmpHrs <= companyEmpWage.maxWorkingHours &&
+                totalWorkingDays < companyEmpWage.noOfWorkingDays) {
             totalWorkingDays++;
-            int empHours;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
-                case IS_FULL_TIME:
-                    empHours = 8;
-                    break;
                 case IS_PART_TIME:
-                    empHours = 4;
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
                     break;
                 default:
-                    empHours = 0;
-                    break;
+                    empHrs = 0;
             }
-            totalEmpHours += empHours;
+            totalEmpHrs += empHrs;
         }
-        return totalEmpHours * companyWage.ratePerHour;
+        return totalEmpHrs * companyEmpWage.ratePerHour;
     }
 
-    /*
-     * This is main method of our program for calling the all method and printing
+    /**
+     * This is main method of our program for calling the all methods and printing
+     * @param args
      */
     public static void main(String[] args) {
+        EmployeeWageBuilder employeeWage = new EmployeeWageBuilder();
         System.out.println("Welcome to employee wage calculator");
-        EmployeeWageBuilder employeeWageBuilder = new EmployeeWageBuilder();
-        employeeWageBuilder.addCompanyEmpWage("D-Mart", 23, 27,80);
-        employeeWageBuilder.addCompanyEmpWage("Reliance", 25, 20, 90);
-        employeeWageBuilder.getMonthlyWages();
+        employeeWage.addCompanyEmployeeWage("D-Mart", 23, 27, 80);
+        employeeWage.addCompanyEmployeeWage("Reliance", 25, 20, 90);
+        employeeWage.computeEmployeeWage();
     }
 }
---
+++ src/com/employeewage/IEmployeeWage.java
@@ -0,0 +1,11 @@
+package com.employeewage;
+
+/**
+ * This is an interface which is implemented by EmployeeWageBuilder class
+ */
+
+public interface IEmployeeWage {
+    void addCompanyEmployeeWage(String Company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth);
+
+    void computeEmployeeWage();
+}
