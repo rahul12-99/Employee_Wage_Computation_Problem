465acccb9ef23a96240b5f8ce8452ec329fcaff5
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-05-12 09:33:48 +0530

    Added : Code for save total wage for each company

--- src/com/employeewage/EmployeeWageBuilder.java
+++ src/com/employeewage/EmployeeWageBuilder.java
@@ -3,45 +3,71 @@ package com.employeewage;
 
 public class EmployeeWageBuilder {
     /**
-     * Initialize the variable which is final and static
+     * Initialize the variable which is final
      */
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
+    private final String Company;
+    private final int empRatePerHour;
+    private final int numOfWorkingDays;
+    private final int maxHoursPerMonth;
+    private int totalEmpWage;
 
     /**
-     * This method is for calculating dailyWage of employee and total wage for multiple company
+     * Created a parametrized constructor for private variable
      */
+    public EmployeeWageBuilder(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+        this.Company = company;
+        this.empRatePerHour = empRatePerHour;
+        this.numOfWorkingDays = numOfWorkingDays;
+        this.maxHoursPerMonth = maxHoursPerMonth;
+    }
 
-    private static void dailyWageOfEmployee(String company, int wage, int numOfWorkingDays, int workingHourPerMonth) {
-        int totalWorkingDays = 0;
-        int totalEmpHrs = 0;
-        int totalWage, empHrs;
-        while (totalWorkingDays < numOfWorkingDays && totalEmpHrs < workingHourPerMonth) {
+    /**
+     * This method is save total wage for company
+     */
+    public void company() {
+        int empHrs, totalWorkingDays = 0, totalEmpHrs = 0;
+
+        while (totalEmpHrs <= maxHoursPerMonth &&
+                totalWorkingDays < numOfWorkingDays) {
+            totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
                 case IS_PART_TIME:
                     empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
                     break;
                 default:
                     empHrs = 0;
             }
-            totalEmpHrs = totalEmpHrs + empHrs;
+            totalEmpHrs += empHrs;
         }
-        totalWage = totalEmpHrs * wage;
-        System.out.println("Employee Wage for " + company + " is = " + totalWage);
+        totalEmpWage = totalEmpHrs * empRatePerHour;
     }
 
     /**
-     * This is main method of our program to call the dailyWageOfEmployee method
-     * @param args
+     * To string method for printing
+     * @return
      */
+    @Override
+    public String toString() {
+        return "Total Emp Wage for " + Company + " = " + totalEmpWage;
+    }
 
+    /**
+     * This is main method of the program for taking input and calling the company method
+     * and printing the total employee wage
+     */
     public static void main(String[] args) {
-        dailyWageOfEmployee("Reliance", 30, 2, 20);
-        dailyWageOfEmployee("V-Mart", 40, 4, 40);
-        dailyWageOfEmployee("D-Mart", 22, 3, 60);
+        EmployeeWageBuilder dMart = new EmployeeWageBuilder("DMart", 23, 25, 80);
+        dMart.company();
+        System.out.println(dMart);
+        EmployeeWageBuilder reliance = new EmployeeWageBuilder("Reliance", 25, 20, 90);
+        reliance.company();
+        System.out.println(reliance);
+
     }
 }
